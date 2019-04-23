6d00495cc0bd1d59c20e675f73a80b6b94e9a5a7
Sheng Chih Su <preztons@gmail.com>
2019-04-22 20:57:49 -0700

    fix

--- python/moveToOrigin.py
+++ python/moveToOrigin.py
@@ -1,8 +1,11 @@
 #moveToOrigin.py
 
+import pymel.core as pm
+
 obj = pm.selected()
 
 box = pm.exactWorldBoundingBox(obj)
 bottom = [(box[0]+ box[3])/2, box[1], (box[2] + box[5])/2]
 pm.xform( obj, piv=bottom, ws=True )
 pm.move( 0, 0, 0, obj, rpr=True, ws=True )
+pm.makeIdentity(apply=True, t=1, r=1, s=1, n=0)
